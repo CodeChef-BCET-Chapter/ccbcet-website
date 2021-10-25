d6ff388f490a2ae9fd0808cbc2ed3dea9b538de0
Wasim Raja <wasim7raja10@gmail.com>
2021-10-25 17:57:13 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './css/tailwind.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   variants: {
     extend: {},
