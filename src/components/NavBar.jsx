ae13a2fafecf2e6c4481bd60f248a19d6409b842
Wasim Raja <wasim7raja10@gmail.com>
2021-10-25 21:28:45 +0530

    given space b/w logo and links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ export default function NavBar() {
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
+
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button */}
             <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
@@ -31,7 +32,8 @@ export default function NavBar() {
               </svg>
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
             <div className="flex-shrink-0 flex items-center">
               <img className="block lg:hidden h-10 w-auto mx-2" src="/img/ccbcet-logo.svg" alt="ccbcet logo" />
               <img class="hidden lg:block h-10 w-auto mx-4" src="/img/ccbcet-logo.svg" alt="Workflow"/>
@@ -39,11 +41,9 @@ export default function NavBar() {
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
-
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                <a href="#" className="navbar-link">Team</a>
+                <a href="#" className="navbar-link">Projects</a>
+                <a href="#" className="navbar-link">Calendar</a>
               </div>
             </div>
           </div>
@@ -53,11 +53,9 @@ export default function NavBar() {
       {/* Mobile menu, show/hide based on menu state. */}
       <div className="sm:hidden" id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-
-          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-
-          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+          <a href="#" className="navbar-link-mobile">Team</a>
+          <a href="#" className="navbar-link-mobile">Projects</a>
+          <a href="#" className="navbar-link-mobile">Calendar</a>
         </div>
       </div>
     </nav>
