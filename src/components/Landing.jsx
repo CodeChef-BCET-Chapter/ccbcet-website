500d238d678837a505bfbfe99effb92083a690f3
Wasim Raja <wasim7raja10@gmail.com>
2021-10-29 23:47:11 +0530

    navbar fixed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <div className="grid lg:grid-cols-2 2xl:grid-cols-5 mb-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mt-12">
+    <div className="grid lg:grid-cols-2 2xl:grid-cols-5 mb-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 md:mt-12">
       <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
         <div className="xl:max-w-xl">
           {/* <img className="h-10" src="/img/ccbcet-logo.svg" alt="Workcation" /> */}
           <img
-            className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+            className="mt-6 rounded-lg sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
             src="/img/landing_img.svg"
             alt="Woman workcationing on the beach"
           />
-          <h1 className="mt-6 text-2xl font-headline tracking-tight font-semibold text-brand-light sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+          <h1 className="mt-12 text-2xl font-headline tracking-tight font-semibold text-brand-light sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
             CodeChef BCET Chapter
           </h1>
           <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
@@ -28,6 +28,7 @@ export default function Landing() {
           </div>
         </div>
       </div>
+      {/* largest view  */}
       <div className="hidden relative lg:block 2xl:col-span-3">
         <img
           className="absolute inset-0 min-w-full h-full"
